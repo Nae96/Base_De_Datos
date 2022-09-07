4204cef6083bb51513d84218859c3b3cddd75019
Nae96 <110498156+Nae96@users.noreply.github.com>
2022-09-07 18:45:14 -0300

    Base De Datos

---
+++ options/MariaDB.js
@@ -0,0 +1,10 @@
+const optionsMariaDB = {
+    client: "mysql",
+    connection: {
+      host: "127.0.0.1",
+      user: "root",
+      database: "ecommerce",
+    },
+  };
+  
+  module.exports = { optionsMariaDB };
